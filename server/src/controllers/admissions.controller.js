e259e79c2c47ef68b1578ba4abb4422948547e65
BrightOlawale <oolawalebright@gmail.com>
2023-08-08 23:22:18 +0100

    feat: get all admissions

---
+++ server/src/controllers/admissions.controller.js
@@ -0,0 +1,27 @@
+import Admissions from '../models/admissions.model.js'
+
+// Create admsission controller class
+export default class AdmissionsController {
+    // Create a static method to get all admissions
+    static async getAllAdmissions(req, res) {
+        try {
+            // Implement Pagination
+            const page = parseInt(req.query.page) || 1
+            const limit = parseInt(req.query.limit) || 10
+            const offset = (page - 1) * limit
+
+            const allAdmissions = await Admissions.findAll({
+                limit: limit,
+                offset: offset
+            })
+            res.status(200).json({
+                message: 'All admissions fetched successfully',
+                admissions: allAdmissions
+            })
+        } catch (error) {
+            res.status(500).json({
+                error: error
+            })
+        }
+    }
+}
