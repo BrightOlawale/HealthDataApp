578eed679beaea6c7e4a72a114e8fa5786d3f8ec
BrightOlawale <oolawalebright@gmail.com>
2023-08-08 23:18:10 +0100

    feat: create model for poe

---
+++ server/src/models/poe.model.js
@@ -0,0 +1,41 @@
+import sequelize from '../config/db.config'
+import Sequelize from 'sequelize'
+
+const Poe = sequelize.define('filtered_poe', {
+    Poe_ID: {
+        type: Sequelize.STRING,
+        primaryKey: true
+    },
+    Poe_Seq: {
+        type: Sequelize.INTEGER
+    },
+    Patient_ID: {
+        type: Sequelize.INTEGER
+    },
+    Admission_ID: {
+        type: Sequelize.INTEGER
+    },
+    Order_Time:{
+        type: Sequelize.DATE
+    },
+    Order_Type: {
+        type: Sequelize.STRING
+    },
+    Order_Subtype: {
+        type: Sequelize.STRING
+    },
+    Transaction_Type: {
+        type: Sequelize.STRING
+    },
+    Discontinue_Of_Poe_ID: {
+        type: Sequelize.STRING
+    },
+    Discontinue_By_Poe_ID: {
+        type: Sequelize.STRING
+    },
+    Order_Status: {
+        type: Sequelize.STRING
+    }
+})
+
+export default Poe
